316d6eacb71df3663c3e133c2022d142ec010d20
George, B. Smith <georgebsmith.tech@gmail.com>
2025-10-03 20:47:35 +0100

    added day 1 class

---
+++ day-1.sql
@@ -0,0 +1,30 @@
+--create database chem_academy1;
+--drop database chem_academy1;
+
+-- create table 
+-- 	users (user_id int,
+-- 			first_name varchar,
+-- 			last_name varchar(10),
+-- 			email varchar(25),
+-- 			dob date,
+-- 			phone varchar(11),
+-- 			role varchar(10),
+-- 			level int)
+
+
+
+-- create table if not exists
+-- 	users (user_id int,
+-- 			"firstName" varchar,
+-- 			last_name varchar(10),
+-- 			email varchar(25),
+-- 			dob date,
+-- 			phone varchar(11),
+-- 			role varchar(10),
+-- 			level int)
+
+-- select * from users
+-- select first_name,dob from users
+
+insert into users values()
+
